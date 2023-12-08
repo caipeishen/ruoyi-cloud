8a18302dc6ca4a541c2437a003daf1afd81c7fb2
caipeishen <peishen.cai@foxmail.com>
2023-12-08 13:22:55 +0800

    knife

--- ruoyi-gateway/src/main/java/com/ruoyi/gateway/config/SwaggerProvider.java
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/config/SwaggerProvider.java
@@ -1,8 +1,12 @@
 package com.ruoyi.gateway.config;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.config.GatewayProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.support.NameUtils;
@@ -37,6 +41,36 @@ public class SwaggerProvider implements SwaggerResourcesProvider, WebFluxConfigu
     private GatewayProperties gatewayProperties;
 
     /**
+     * 网关应用名称
+     */
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+//    /**
+//     * 聚合其他服务接口
+//     *
+//     * @return
+//     */
+//    @Override
+//    public List<SwaggerResource> get()
+//    {
+//        List<SwaggerResource> resourceList = new ArrayList<>();
+//        List<String> routes = new ArrayList<>();
+//        // 获取网关中配置的route
+//        routeLocator.getRoutes().subscribe(route -> routes.add(route.getId()));
+//        gatewayProperties.getRoutes().stream()
+//                .filter(routeDefinition -> routes
+//                        .contains(routeDefinition.getId()))
+//                .forEach(routeDefinition -> routeDefinition.getPredicates().stream()
+//                        .filter(predicateDefinition -> "Path".equalsIgnoreCase(predicateDefinition.getName()))
+//                        .filter(predicateDefinition -> !"ruoyi-auth".equalsIgnoreCase(routeDefinition.getId()))
+//                        .forEach(predicateDefinition -> resourceList
+//                                .add(swaggerResource(routeDefinition.getId(), predicateDefinition.getArgs()
+//                                        .get(NameUtils.GENERATED_NAME_PREFIX + "0").replace("/**", SWAGGER2URL)))));
+//        return resourceList;
+//    }
+
+    /**
      * 聚合其他服务接口
      * 
      * @return
@@ -44,20 +78,29 @@ public class SwaggerProvider implements SwaggerResourcesProvider, WebFluxConfigu
     @Override
     public List<SwaggerResource> get()
     {
-        List<SwaggerResource> resourceList = new ArrayList<>();
-        List<String> routes = new ArrayList<>();
-        // 获取网关中配置的route
-        routeLocator.getRoutes().subscribe(route -> routes.add(route.getId()));
-        gatewayProperties.getRoutes().stream()
-                .filter(routeDefinition -> routes
-                        .contains(routeDefinition.getId()))
-                .forEach(routeDefinition -> routeDefinition.getPredicates().stream()
-                        .filter(predicateDefinition -> "Path".equalsIgnoreCase(predicateDefinition.getName()))
-                        .filter(predicateDefinition -> !"ruoyi-auth".equalsIgnoreCase(routeDefinition.getId()))
-                        .forEach(predicateDefinition -> resourceList
-                                .add(swaggerResource(routeDefinition.getId(), predicateDefinition.getArgs()
-                                        .get(NameUtils.GENERATED_NAME_PREFIX + "0").replace("/**", SWAGGER2URL)))));
-        return resourceList;
+        //接口资源列表
+        List<SwaggerResource> resources = new ArrayList<>();
+        //服务名称列表
+        List<String> routeHosts = new ArrayList<>();
+        // 获取所有可用的应用名称
+        routeLocator.getRoutes().filter(route -> route.getUri().getHost() != null)
+                .filter(route -> !applicationName.equals(route.getUri().getHost()))
+                .subscribe(route -> routeHosts.add(route.getUri().getHost()));
+        //去重，多负载服务只添加一次
+        Set<String> existsServer = new HashSet<>();
+        routeHosts.forEach(host -> {
+            // 拼接url
+            String url = "/" + host + SWAGGER2URL;
+            //不存在则添加
+            if (!existsServer.contains(url)) {
+                existsServer.add(url);
+                SwaggerResource swaggerResource = new SwaggerResource();
+                swaggerResource.setUrl(url);
+                swaggerResource.setName(host);
+                resources.add(swaggerResource);
+            }
+        });
+        return resources;
     }
 
     private SwaggerResource swaggerResource(String name, String location)
